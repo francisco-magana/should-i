beac233e2c9b0502f1ac55a7f75f2b5b30b48bea
francisco-magana <franciscomagana.contact@gmail.com>
2023-08-27 21:08:29 -0600

    add question component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './components/ui/title/title.component';
+import { QuestionComponent } from './components/question/question.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent
+    TitleComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
