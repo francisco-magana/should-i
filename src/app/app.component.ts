948324799c0026e92c3180d2f5f682262741af06
francisco-magana <franciscomagana.contact@gmail.com>
2023-08-27 23:00:43 -0600

    add initi questions feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+enum steps {
+  SHOW_TITLE = 1,
+  QUESTIONS = 2
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'should-i';
+  steps = steps;
+  current_step: number = steps.SHOW_TITLE;
+
+  startQuestions() {
+    this.current_step = this.steps.QUESTIONS;
+  }
+
 }
